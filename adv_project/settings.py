a8339d0fd2498f2a282eee790f4b0cde61065b6a
quansenB <inaki.breinbauer@gmail.com>
2019-10-22 11:32:54 +0200

    save more changes

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -101,19 +101,8 @@ WSGI_APPLICATION = 'adv_project.wsgi.application'
     }
 } """
 
-""" DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mudDB',
-        'USER': 'Lambda',
-        'PASSWORD': 'supersecurepassword',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-} """
-
- DATABASES = {}
- DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
